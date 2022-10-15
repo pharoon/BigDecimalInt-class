bebcdf03055dec45127b8bdd0e84ea02c30ba0f8
pharoon <66510216+pharoon@users.noreply.github.com>
2022-10-15 21:24:05 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
 #include <string>
 #include <regex>
-using namespace std;
+void sizeBalance(std::string &str1, std::string &str2){
+    std::string zero{"0"};
+    while (str1.size() < str2.size()){
+        str1 = zero + str1;
+    }
+    while (str1.size() > str2.size()){
+        str2 = zero + str2;
+    }
+}
+
+std::string adding_strNums(std::string &str1, std::string &str2){
+    size_t sz{str1.size()-1};
+    std::string res;
+    int carry{0};
+    for (int i = 0; i <= sz; ++i){
+
+        if ((((int)str1[sz - i] - 48) + ((int)str2[sz - i] - 48) + carry) > 9){
+            res = std::to_string(carry + ( (int) str1[sz - i] - 48) + ((int) str2[sz - i] - 48) - 10) + res ;
+            carry = 1;
+        }
+        else {
+            res = std::to_string(carry + ( (int) str1[sz - i] - 48) + ((int) str2[sz - i] - 48)) + res ;
+            carry = 0;
+        }
+    }
+    if (carry == 1){
+        res = "1" + res;
+    }
+    return res;
+}
+
 
 class BigDecimal {
     //Member variables
@@ -11,7 +41,7 @@ class BigDecimal {
 
 public :
     //Constructor to_string
-    //BigDecimal() = default;
+    BigDecimal() = default;
 
     explicit BigDecimal(int num_param){
 
@@ -30,9 +60,8 @@ public :
     explicit BigDecimal(std::string num_param){
         std::regex filter("(\\+|-)?[0-9]+");
         if (!std::regex_match(num_param,filter)) {
-            // throws error if the input is invalid (to make it a real BigDecimal class) 
             throw std::invalid_argument("number");
-            
+            // or throw std::out_of_range("month");
         }
         else{
             if (num_param[0] == '+' || num_param[0] == '-') {
@@ -48,7 +77,6 @@ public :
     }
 
     //Functions (methods)
-
 
     //getter methods
     size_t get_size(){
@@ -74,67 +102,28 @@ public :
     }
 
     //operators methods
-    
-   friend ostream &operator<<(ostream &output, BigDecimal &x) {
-        output << x.num;
-        return output;
-    }
-    
-     bool operator>(BigDecimal &dec2) {
-        if (dec2.num[0] == '-' && num[0] == '+') { 
-            return true; 
-        } else if (num[0] == '-' && dec2.num[0] == '+') {
-            return false;
-        } else if (num[0] == '+' && dec2.num[0] == '+') {
-            if (num.length() < dec2.num.length()) {
-                return false;
-            }
-            for (int i = 0; i < num.length(); i++) {
-                if (num[i] > dec2.num[i]) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        } else if (num[0] == '-' && dec2.num[0] == '-') {
-            if (num.length() < dec2.num.length()) {
-                return true;
-            }
-            for (int i = 0; i < num.length(); i++) {
-                if (num[i] < dec2.num[i]) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
+    BigDecimal operator + (BigDecimal num2) {
+        BigDecimal res{"0"};
+
+        if (this->sign == '+' && num2.sign == '+'){
+            res.sign = '+';
+            sizeBalance(this->num, num2.num);
+            res.num = adding_strNums(this->num, num2.num);
+            std::cout << res.num << std::endl;
         }
-    }
-     int sgn()
-    {
-        if(num[0]=='+')
-        {
-            return 1;
+        else if (this->sign == '-' && num2.sign == '-') {
+            res.sign = '-';
+            sizeBalance(this->num, num2.num);
+            res.num = adding_strNums(this->num, num2.num);
+            std::cout << res.num << std::endl;
+
         }
-        else if(num[0]=='-')
-        {
-            return 0;
+        else{
+            std::cout << "mlna4 da3wa 4o8l kariem :)" << std::endl;
+            std::cout << res.num << std::endl;
+
         }
-    }
-    int sze()
-    {
-        return num.length();
-    } 
-    
-    bool operator==(BigDecimal a)
-    {
-        for (int i = 0; i < num.length(); ++i) {
-            if (num == a.num) {
-                continue;
-            } else {
-                return false;
-            }
-        }
-        return true;
+        return res;
     }
 
 };
@@ -142,12 +131,27 @@ public :
 int main() {
     std::cout << "hi ^_^" << std::endl;
     int m {+123};
-    std::string mmm{"+123"};
+    std::string mmm{"123"},mm{"589999"};
     BigDecimal kkk(mmm);
     std::cout << kkk.get_num() << std::endl;
     std::cout << kkk.get_sign() << std::endl;
     std::cout << kkk.get_size() << std::endl;
-    std::cout << "end of main bye ^_^" << std::endl;
+ 
+    BigDecimal num1("123456789012345678901234567890");
+    BigDecimal num2("+113456789011345678901134567890");
+    BigDecimal num3("-200000000000000000000000000000");
+    std::cout << num1.get_num() << std::endl;
+    std::cout << num2.get_num() << std::endl;
+    std::cout << num3.get_num() << std::endl;
+    BigDecimal num4 = num2 + num1;
+    std::cout << num4.get_num() << std::endl;
+    std::cout << "the end bye ^_^" << std::endl;
+    /*std::cout << "num1 = " << num1 << std::endl;
+    std::cout << "num2 = " << num2 << estd::ndl;
+//Next statement will print 236913578023691357802369135780
+    std::cout << "num2 + num1 = " << num4 << std::endl;
+//Next statement will print -313456789011345678901134567890
+    std::cout << "num2 - num1 = " << std::num5 << std::endl;
+*/
 
-    return 0;
 }
